56bba9786fe59f384687d10bd5a2d722a6497d48
Sanjeev1420 <sanjeevrajspkec2002@gmail.com>
2024-04-09 15:35:02 +0530

    the token validation for getAllBRands , getProductsForInvoice and getLowStockProducts where disabled

--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -7,15 +7,15 @@ import ProductController from "../controllers/product.js"
 const router = express.Router()
 
 router.post('/addBrand' , TokenValidation, commonUploadMiddleware ,BrandController.addBrand)
-router.get('/getAllBrands' , TokenValidation , BrandController.getAllBrand)
+router.get('/getAllBrands', BrandController.getAllBrand)
 router.put('/updateBrand' , TokenValidation , BrandController.updateBrand)
 router.delete('/deleteBrand' , TokenValidation ,BrandController.deleteBrand)
 router.get('/getProductsByBrandId' , TokenValidation , ProductController.getProductsByBrandId)
 router.post('/addProduct', TokenValidation , commonUploadMiddleware , ProductController.addProduct )
 router.put('/updateProduct' , TokenValidation , ProductController.updateProduct)
 router.delete('/deleteProduct' , TokenValidation ,ProductController.deleteProduct)
-router.get('/getProductsForInvoice' , TokenValidation , ProductController.getProductsForInvoice)
-router.get('/getLowStockProducts' , TokenValidation , ProductController.getLowStockProducts)
+router.get('/getProductsForInvoice' , ProductController.getProductsForInvoice)
+router.get('/getLowStockProducts' , ProductController.getLowStockProducts)
 
 export default router
 
